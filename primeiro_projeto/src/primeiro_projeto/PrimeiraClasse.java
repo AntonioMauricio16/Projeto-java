1f6df2e568dc5b6f1840bcbf6719fb7abda71400
antonio <antonio@DESKTOP-EJKEQ25>
2023-06-05 17:32:42 -0300

    criando  comentarios

--- primeiro_projeto/src/primeiro_projeto/PrimeiraClasse.java
+++ primeiro_projeto/src/primeiro_projeto/PrimeiraClasse.java
@@ -2,8 +2,9 @@ package primeiro_projeto;
 
 public class PrimeiraClasse {
 	
-	public static void main(String [] args) {
-		System.out.println("Vamos na no Back-and!");
+	public static void main(String [] args) { // serve para executar o codigo
+		System.out.println("Vamos na no Back-and!"); // serve para mostra na tela
+		
 	}
 
 }
